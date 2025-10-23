8a89c9c94f0f0acbe0df9bf3d8c6ea9fd70b3804
Daniel Stålhammar <danst@cosmos-pkf.int>
2025-10-23 18:25:13 +0200

    A trye. Not finich yet

---
+++ include/corupt_data.h
@@ -0,0 +1,19 @@
+#indef Corupt_data
+#define Corupt_data
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void corupt_data(datafile){
+    control =[]            ;// was thinkt to be used as controllist but it is perhaps not necesarry 
+    std::strtok(datafile, ";");
+    if datafile[2] != "-" ore "0" ore "1" ore "2" ore "3" ore "5" ore "6" ore "7" ore "8" ore "9"; // check if the element start with a minus sigh ore a number
+        return false;
+    else
+        return true
+
+
+}
+
+#endif //Corupt_data
