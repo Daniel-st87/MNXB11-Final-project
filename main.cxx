f08cc87a8939df1de0afa0fd90124f11a302c65e
Marcus O'Riordan <marcus.oriordan4@gmail.com>
2025-10-29 15:21:04 +0100

    Made the Makefile and changed the main.cxx so it will run all 4 graphs

---
+++ main.cxx
@@ -0,0 +1,26 @@
+#include "average_temp.h"
+#include "filter_date.h"
+#include "events_per_year.h"
+#include "decade_average.h"
+#include "compare_temps.h"
+#include "make_lists.h"
+#include "max_temp_per_year.h"
+#include "waves_uppsala_lund.h"
+#include "temp_analysis.h"
+#include "main_minmax.h"
+#include "main_max_temp.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Generating graphs..." << std::endl;
+
+    compare_temps();     // For temps between two cities
+    minmax();            // For min and max temp
+    waves();             // for the heat waves figures
+    main_max_Halmstad(); // For creating the max temps in halmstad
+
+    std::cout << "All graphs completed!" << std::endl;
+    return 0;
+}
